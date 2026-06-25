ecf3533167be355c2c0dd4f1edd4f9f88bc4f586
sachinmarmat <sb6583425@gmail.com>
2026-06-25 22:40:01 +0530

    sdf

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({ 
   origin: [
     process.env.CLIENT_URL || 'http://localhost:5173',
-    'https://restaurant-f-alpha.vercel.app/'
+    'https://restaurant-f-alpha.vercel.app'
   ], 
   credentials: true 
 }));app.use(express.json());
